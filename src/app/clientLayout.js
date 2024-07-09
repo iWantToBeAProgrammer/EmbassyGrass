83573f6cdb78df419f8ac45246e0fae24fff83d8
iWantToBeAProgrammer <juanstevenson10@gmail.com>
2024-07-10 01:15:37 +0700

    page with use client

--- src/app/clientLayout.js
+++ src/app/clientLayout.js
@@ -1,23 +1,10 @@
 "use client";
 
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
-import { adjustLayoutForScale } from "./utils/AdjustLayout"; // Adjust the import path as needed
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function ClientLayout({ children }) {
-  // const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-  // if (isMobile) {
-  //   useEffect(() => {
-  //     adjustLayoutForScale();
-  //     window.addEventListener("resize", adjustLayoutForScale);
-  //     return () => {
-  //       window.removeEventListener("resize", adjustLayoutForScale);
-  //     };
-  //   }, []);
-  // }
 
   return <body className={inter.className}>{children}</body>;
 }
--- src/app/utils/AdjustLayout.js
+++ src/app/utils/AdjustLayout.js
@@ -1,10 +1,1 @@
-// export function adjustLayoutForScale() {
-//   const scale = window.devicePixelRatio;
-
-//     if (scale > 1) {
-//       document.documentElement.style.fontSize = `${16 / scale}px`;
-//     } else {
-//       document.documentElement.style.fontSize = "16px";
-//     }
-//   }
 
